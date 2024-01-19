88d2e1cdd8e000141c35500109384631ba369d23
Atharva Naik <atharvan.coder@gmail.com>
2024-01-19 12:17:05 +0530

    Fixing Production issues atp-5

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,17 @@ import Link from "next/link";
 import AuthLinks from "@/components/authLinks/AuthLinks";
 import ThemeToggle from "@/components/themeToggle/ThemeToggle";
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Blog</div>
-      <div className={styles.links}>
-        <ThemeToggle />
-        <Link href="/" className={styles.link}>Home</Link>
-        <Link href="/" className={styles.link}>About</Link>
-        <AuthLinks />
-      </div>
+    <div className={styles.logo}>Blog</div>
+    <div className={styles.links}>
+      <ThemeToggle />
+      <Link href="/" className={styles.link}>Home</Link>
+      <Link href="/" className={styles.link}>About</Link>
+      <AuthLinks />
     </div>
-  );
-};
+  </div>
+  )
+}
 
-export default Navbar;
